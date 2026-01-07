5d1af55b12d89e0582f136f21fb487d311bf3ad9
Jaire Marques <jaire.marques@rfs.pt>
2026-01-07 10:46:55 +0000

    add staticfiles in the settings

--- glowseven_api/settings/base.py
+++ glowseven_api/settings/base.py
@@ -93,6 +93,7 @@ USE_TZ = True
 # Static / Media
 # ------------------------------------------------------------------------------
 STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
